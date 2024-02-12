6bfdf670896f10fad3f63f6e3b879e6770476728
Dhruv Dave <heydhruvdave@gmail.com>
2024-02-12 17:42:04 +0530

    DRF response and decorators

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,14 @@
-from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from products.models import Product
+from django.forms.models import model_to_dict
 import json
+
+@api_view(["GET", "POST"])
 def home_api(request, *args, **kwargs):
     model_data = Product.objects.all().order_by('?').first()
-    data= {}
     if model_data:
-        data["title"] = model_data.title
-        data["content"] = model_data.content
-        data["price"] = model_data.price
-    return JsonResponse(data)
+        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
+    else :
+        data = None
+    return Response(data)
