5964bfbc7f4024abcf89ce1043a8416f92172530
Dhruv Dave <heydhruvdave@gmail.com>
2024-02-15 01:43:36 +0530

    create update serializers

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,13 +1,31 @@
 from rest_framework import serializers
-
+from rest_framework.reverse import reverse
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
+    update_url = serializers.HyperlinkedIdentityField(
+        view_name='product-update',
+        lookup_field='pk'
+    )
+    email = serializers.EmailField(write_only=True)
     class Meta:
         model = Product
         fields = [
+            'update_url',
+            'url',
             'title',
             'content',
             'price',
             'sale_price',
+            'email',
         ]
+
+    def create(self, validated_data): # just to understand create of seri..
+        email = validated_data.pop('email')
+        obj = super().create(validated_data)
+        return obj
+    def get_url(self, obj):
+        request = self.context.get('request')
+        if request is None:
+            return None
+        return reverse("product-list", kwargs={"pk": obj.pk}, request=request)
--- backend/products/urls.py
+++ backend/products/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('<int:pk>/', views.ProductDetailsApiView.as_view()),
-    path('<int:pk>/update/', views.ProductUpdateApiView.as_view()),
+    path('<int:pk>/update/', views.ProductUpdateApiView.as_view(), name='product-update'),
     path('<int:pk>/delete/', views.ProductDeleteApiView.as_view()),
-    path('', views.ProductListCreateApiView.as_view())
+    path('', views.ProductListCreateApiView.as_view(), name="product-list")
 ]
